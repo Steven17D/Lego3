3467cc4625af3274a05cc20fb041de7ea52e5bf1
Steven Dahsevsky <dashevskysteven@gmail.com>
2020-03-25 19:36:19 +0200

    cleaned up plugin

--- lego/pytest_lego/plugin.py
+++ lego/pytest_lego/plugin.py
@@ -1,33 +1,48 @@
-import pytest
+import rpyc
+
+MARK = "lego"
 
 
-def pytest_generate_tests(metafunc):
-    if "slave" in metafunc.fixturenames:
-        metafunc.parametrize("slave", next(metafunc.definition.iter_markers()).args)
+
+def prepare_slave(slave_hostname):
+    resource_manager = rpyc.connect(host='central', port=18861)
+    resource_manager.root.
+    connection = rpyc.classic.connect(slave_hostname, keepalive=True)
+    return
 
 
-def pytest_runtest_setup(item: pytest.Item):
-    # a = item.ihook.pytest_runtest_call
-    #
-    # def f(*args, **kwargs):
-    #     print("Before")
-    #     print(f"a = {a}, args = {args}, kwargs={kwargs}")
-    #     a(*args, **kwargs)
-    #     print("After")
-    #
-    # item.ihook.pytest_runtest_call = f
-    # print("setting up", item)
-    # for mark in item.iter_markers():
-    #     print(f"Mark is {mark}")
+def pytest_cmdline_parse(pluginmanager, args):
     pass
 
 
-def pytest_pyfunc_call(pyfuncitem):
-    test_function = pyfuncitem.obj
+def pytest_generate_tests(metafunc):
+    """
+    Generate (multiple) parametrized calls to a test function.
 
-    def wrapped_test(*args, **kwargs):
-        slave = OSError(kwargs["slave"])
-        test_function(slave)
+    :param metafunc: _pytest.python.Metafunc
+    :return:
+    """
 
-    pyfuncitem.obj = wrapped_test
+    # next((mark for mark in metafunc.definition.iter_markers() if MARK == mark.name), None)
+    lego_mark = metafunc.definition.get_closest_marker(MARK)
+    if lego_mark is None:
+        # The test doesn't have lego mark
+        return
+
+    assert ["slave"] == metafunc.fixturenames, "Lego test must have argument 'slave'"
+    metafunc.parametrize("slave", next(metafunc.definition.iter_markers()).args)
+
+
+def pytest_runtest_setup(item: pytest.Item):
+    lego_mark = item.get_closest_marker(MARK)
+    if lego_mark is None:
+        # The test doesn't have lego mark
+        return
+
+    test_function = item.obj
+
+    def test_wrapper(*args, **kwargs):
+        test_function(slave=prepare_slave(*args, **kwargs))
+
+    item.obj = test_wrapper
     pass
--- test.py
+++ test.py
@@ -1,17 +1,28 @@
 import pytest
-import socket
-import asyncio
-import lego
 import time
 
 
 @pytest.mark.lego("1.1.1.1")
 def test_a(slave):
     print(f"Slave a: {slave}")
-    time.sleep(2)
+    time.sleep(1)
 
 
 @pytest.mark.lego("2.2.2.2")
 def test_b(slave):
     print(f"Slave b: {slave}")
-    time.sleep(2)
+    time.sleep(1)
+
+
+@pytest.mark.xfail
+def test_missing_fixture(slave):
+    print(f"Slave a: {slave}")
+    time.sleep(1)
+
+
+# @pytest.mark.xfail
+# @pytest.mark.lego("2.2.2.2")
+# def test_missing_argument(test_input):
+#     print(f"No slave")
+#     time.sleep(1)
+
