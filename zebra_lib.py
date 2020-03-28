64b177377500205a792282906cb4136a49df0b80
elyash <elyashiv12@gmail.com>
2020-03-28 17:39:38 +0000

    Fixed CR 1

--- zebra_lib.py
+++ zebra_lib.py
@@ -20,10 +20,6 @@ class EventHandler(watchdog.events.FileSystemEventHandler):
 class Lib:
     def __init__(self, con):
         self._con = con
-        self._bgsrv = rpyc.BgServingThread(self._con)
-
-    def __del__(self):
-        self._bgsrv.stop()
 
 
 class ZebraLib(Lib):
@@ -31,17 +27,17 @@ class ZebraLib(Lib):
         super(ZebraLib, self).__init__(con)
 
     def send_packets(self, *args, **kwargs):
-        sender = rpyc.async_(self._con.modules['scapy.all'].send)
-        return sender(*args, **kwargs)
+        r_sender = rpyc.async_(self._con.modules['scapy.all'].send)
+        return r_sender(*args, **kwargs)
 
     @contextmanager
     def sniff_packets(self, *args, **kwargs):
-        sniffer = self._con.modules['scapy.all'].AsyncSniffer(*args, **kwargs)
-        sniffer.start()
+        r_sniffer = self._con.modules['scapy.all'].AsyncSniffer(*args, **kwargs)
+        r_sniffer.start()
         try:
-            yield sniffer
+            yield r_sniffer
         finally:
-            sniffer.stop()
+            r_sniffer.stop()
 
     @contextmanager
     def monitor_logs(
@@ -49,30 +45,32 @@ class ZebraLib(Lib):
             event_handler: watchdog.events.FileSystemEventHandler,
             files: list
         ):
-        observer = self._con.modules['watchdog.observers'].Observer()
+        r_observer = self._con.modules['watchdog.observers'].Observer()
         for file in files:
-            observer.schedule(event_handler, file)
-        observer.start()
+            r_observer.schedule(event_handler, file)
+        r_observer.start()
         try:
-            yield observer
+            yield r_observer
         finally:
-            observer.stop()
+            r_observer.stop()
 
 def main():
     con = rpyc.classic.connect('127.0.0.1')
+    bgsrv = rpyc.BgServingThread(con)
     z = ZebraLib(con)
     f = open('a.txt', 'w')
 
     with z.sniff_packets(filter='udp and port 1337'):
-        ip = con.modules['scapy.all'].IP
-        udp = con.modules['scapy.all'].UDP
-        z.send_packets(ip(dst='127.0.0.1')/udp(dport=1338))
+        r_ip = con.modules['scapy.all'].IP
+        r_udp = con.modules['scapy.all'].UDP
+        z.send_packets(r_ip(dst='127.0.0.1')/r_udp(dport=1338))
 
     with z.monitor_logs(EventHandler(), ['.']) as observer:
         f.write('Lego 3 will be great')
         f.flush()
 
     f.close()
+    bgsrv.stop()
     con.close()
 
 
