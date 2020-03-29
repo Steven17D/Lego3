c46b5908529395beb41f6225883c12c8ed8dc4d7
elyash <elyashiv12@gmail.com>
2020-03-29 08:55:20 +0000

    Pylinted libs

--- zebra_lib.py
+++ zebra_lib.py
@@ -1,12 +1,7 @@
 """Zebra lib is API to Zebra component."""
 
-import sys
-import rpyc
-import time
-import socket
 import random
 import asyncio
-from contextlib import contextmanager
 
 import core_lib
 
@@ -14,25 +9,24 @@ import core_lib
 class ZebraLib(core_lib.CoreLib):
     """An extended library for Zebra component."""
 
-    def __init__(self, con):
-        super(ZebraLib, self).__init__(con)
-
-    async def send_and_recive(self, ip: str, port: int, count: int=5):
+    async def send_and_recive(self, dst_ip: str, dst_port: int, count: int = 5):
         """Sends packets and receive them back.
 
         Args:
-            ip: The IP to send to and receive from.
-            port: The port to send to and receive from.
+            dst_ip: The IP to send to and receive from.
+            dst_port: The port to send to and receive from.
             count: The number of packets to send.
         """
 
         src_port = random.randint(10000, 20000)
-        r_IP = self._con.modules['scapy.all'].IP
-        r_UDP = self._con.modules['scapy.all'].udp
-        packet = r_IP(dst=ip)/r_UDP(sport=src_port, dport=port)/'Lego3 is great'
+        packet = (
+            self._con.modules['scapy.all'].IP(dst=dst_ip) /
+            self._con.modules['scapy.all'].UDP(sport=src_port, dport=dst_port)/
+            'Lego3 is great'
+        )
         r_send = self._con.modules['scapy.all'].send
         r_sniffer = self._con.modules['scapy.all'].AsyncSniffer(
-            filter=f'udp and src port {port} and dst port {src_port}', count=count)
+            filter=f'udp and src port {dst_port} and dst port {src_port}', count=count)
 
         r_sniffer.start()
         r_send(packet, count=count)
