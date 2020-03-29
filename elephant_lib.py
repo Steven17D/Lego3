24d636377016487258314c044f040ef7cbf66cf3
elyash <elyashiv12@gmail.com>
2020-03-29 08:50:17 +0000

    Fixed elephant and zebra libs

--- elephant_lib.py
+++ elephant_lib.py
@@ -1,5 +1,6 @@
+"""Elephant lib is API to elephant component."""
+
 import sys
-import lib
 import rpyc
 import time
 import socket
@@ -7,32 +8,34 @@ import random
 import asyncio
 from contextlib import contextmanager
 
+import core_lib
 
-class ElephantLib(lib.Lib):
+
+class ElephantLib(core_lib.CoreLib):
+    """An extended library for Elephant component."""
+
     def __init__(self, con):
         super(ElephantLib, self).__init__(con)
 
-    def send_packets(self, *args, **kwargs):
-        r_sender = rpyc.async_(self._con.modules['scapy.all'].send)
-        return r_sender(*args, **kwargs)
+    async def send_and_recive(self, ip: str, port: int, count: int=5):
+        """Sends packets and receive them back.
 
-    @contextmanager
-    def sniff_packets(self, *args, **kwargs):
-        r_sniffer = self._con.modules['scapy.all'].AsyncSniffer(*args, **kwargs)
-        r_sniffer.start()
-        try:
-            yield r_sniffer
-        finally:
-            r_sniffer.stop()
+        Args:
+            ip: The IP to send to and receive from.
+            port: The port to send to and receive from.
+            count: The number of packets to send.
+        """
 
-    async def send_and_recive(self, ip, port, count=5):
         src_port = random.randint(10000, 20000)
         r_IP = self._con.modules['scapy.all'].IP
         r_UDP = self._con.modules['scapy.all'].udp
         packet = r_IP(dst=ip)/r_UDP(sport=src_port, dport=port)/'Lego3 is great'
+        r_send = self._con.modules['scapy.all'].send
+        r_sniffer = self._con.modules['scapy.all'].AsyncSniffer(
+            filter=f'udp and src port {port} and dst port {src_port}', count=count)
 
-        with self.sniff_packets(filter=f'udp and src port {port} and dst port {src_port}', count=count) as sniffer:
-            self.send_packets(packet, count=count)
-            await asyncio.sleep(3)
-            assert len(sniffer.join()) != count
-
+        r_sniffer.start()
+        r_send(packet, count=count)
+        await asyncio.sleep(3)
+        packets = r_sniffer.stop()
+        assert len(packets) == count
