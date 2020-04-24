6d534bce7659d015a2981b190ed5f7da720978d7
Yuval Krinsky <ykrinsky@gmail.com>
2020-04-24 22:35:00 +0300

    Fixed Chine's CR: moved .mark.lego to base class.

--- example/test_tetanus.py
+++ example/test_tetanus.py
@@ -20,6 +20,7 @@ class TestsSpecGiraffe:
     _giraffe: Giraffe = None # type: ignore
 
     @classmethod
+    @pytest.mark.lego('giraffe.bob')
     def setup_class(cls, components: List[Giraffe]) -> None:
         """Initializes the variables once in the start of the spec.
 
@@ -41,36 +42,32 @@ class TestsSpecTetanus(TestsSpecGiraffe):
     _tetanus_lib: Tetanus
     _echo_port: int
 
-    @classmethod
-    @pytest.mark.lego('giraffe.bob')
-    def setup_class(cls, components: List[Giraffe]) -> None:
-        """Initializes the variables once in the start of the spec.
+    @pytest.fixture(scope='class', autouse=True)
+    def init_tetanus_lib(self) -> None:
+        """Initializes tetanus lib, only once, in the start of the test suite."""
 
-        Args:
-            components: Required components.
-        """
-
-        super().setup_class(components)
-
-        cls._tetanus_lib = Tetanus()
-        cls._echo_port = 1337
+        # Can't set instance attributes in fixture with scope='class', only class attributes.
+        cls = type(self)
+        cls._tetanus_lib = Tetanus()  # pylint: disable=protected-access
+        cls._echo_port = 1337  # pylint: disable=protected-access
 
     @pytest.fixture(scope='function', autouse=True, params=[TEST_VERSION])
     def tetanus(self, request):  # type: ignore
         """Install Tetanus before every test function, and uninstall after test end."""
+
         tetanus_version = request.param
         # Install Tetanus at the start of each test.
-        self._tetanus_lib.install(TestsSpecTetanus._giraffe, tetanus_version, self._echo_port)
+        self._tetanus_lib.install(self._giraffe, tetanus_version, self._echo_port)
         yield
         # Uninstall Tetanus at the end of each test.
-        self._tetanus_lib.uninstall(TestsSpecTetanus._giraffe)
+        self._tetanus_lib.uninstall(self._giraffe)
 
     @pytest.mark.lego('zebra.alice')
     async def test_send_and_recv(self, components):  # type: ignore
         """Send packets and expect them back."""
 
         zebra, *_ = components
-        await zebra.send_and_receive(TestsSpecTetanus._giraffe.get_ip(), self._echo_port)
+        await zebra.send_and_receive(self._giraffe.get_ip(), self._echo_port)
 
     @pytest.mark.lego('zebra.alice and zebra.logan')
     async def test_multi_send_and_recv(self, components):  # type: ignore
@@ -80,7 +77,7 @@ class TestsSpecTetanus(TestsSpecGiraffe):
 
         for component in components:
             tasks.append(asyncio.ensure_future(
-                component.send_and_receive(TestsSpecTetanus._giraffe.get_ip(), self._echo_port)))
+                component.send_and_receive(self._giraffe.get_ip(), self._echo_port)))
 
         await asyncio.gather(*tasks)
 
@@ -90,9 +87,18 @@ class TestsSpecTetanus(TestsSpecGiraffe):
 
         zebra, *_ = components
         with TestsSpecTetanus._giraffe.monitor_logs(event_handler=None, directory='.'):
-            await zebra.send_and_receive(TestsSpecTetanus._giraffe.get_ip(), self._echo_port)
+            await zebra.send_and_receive(self._giraffe.get_ip(), self._echo_port)
 
     @pytest.mark.lego('zebra.alice and zebra.logan')
+    # Example of cool usage of parametrize.
+    @pytest.mark.parametrize(
+        'tetanus',
+        [
+            pytest.param(1, marks=pytest.mark.xfail),
+            pytest.param(2, marks=pytest.mark.xfail),
+            3
+        ],
+        indirect=True)
     async def test_multi_monitor_send_and_receive(self, components):  # type: ignore
         """Send and receive data from multiple components, while validating no bad logs written."""
 
@@ -100,7 +106,7 @@ class TestsSpecTetanus(TestsSpecGiraffe):
 
         for component in components:
             tasks.append(asyncio.ensure_future(
-                component.send_and_receive(TestsSpecTetanus._giraffe.get_ip(), self._echo_port)))
+                component.send_and_receive(self._giraffe.get_ip(), self._echo_port)))
 
         with TestsSpecTetanus._giraffe.monitor_logs(event_handler=None, directory='.'):
             await asyncio.gather(*tasks)
