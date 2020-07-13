8573f32b22021046d1d527ae7b2268c3d63e277d
elyash <elyashiv12@gmail.com>
2020-07-13 18:30:50 +0000

    Moved file monitoring into Giraffe

--- example/components/giraffe.py
+++ example/components/giraffe.py
@@ -1,35 +1,11 @@
 """Giraffe component is the API to Giraffe component."""
-from typing import Any
+from typing import Any, Optional
 
 import asyncio
 import contextlib
-import rpyc
+import os
 
 from Lego3.lego.components import RPyCComponent
-
-
-class LogMonitor:
-    """A logs monitoring manager."""
-
-    async def monitor(
-            self,
-            connection: rpyc.Connection,
-            path: str
-        ) -> Any:
-        """Monitor file task.
-
-        Args:
-            connection: The RPyC connection to remove mechine.
-            path: The file or directory to monitor.
-        """
-
-        try:
-            first_stat = connection.modules.os.stat(path)
-            while True:
-                assert first_stat == connection.modules.os.stat(path)
-                await asyncio.sleep(0)
-        except asyncio.CancelledError:
-            pass
 
 
 class Giraffe(RPyCComponent):
@@ -43,8 +19,7 @@ class Giraffe(RPyCComponent):
             path: The file or directory to monitor.
         """
 
-        logs_monitor = LogMonitor()
-        monitoring = asyncio.create_task(logs_monitor.monitor(self.connection, path))
+        monitoring = asyncio.create_task(self._monitor_logs(path))
         yield
         try:
             monitoring.result() # Raises the task exceptions
@@ -52,3 +27,43 @@ class Giraffe(RPyCComponent):
             pass
         finally:
             monitoring.cancel()
+
+    async def _monitor_logs(self, path: str) -> Any:
+        """Monitors on file or directory in this remove machine.
+
+        This is an asyncio task.
+
+        Args:
+            path: The file or directory to monitor.
+        """
+
+        # Task Initialization
+        r_os = self.connection.modules.os
+        last_stat = r_os.stat(path)
+        log_file = self.connection.builtin.open(path, 'r')
+        last_parsed_point = log_file.seek(0, r_os.SEEK_END)
+
+        # Task Loop
+        try:
+            while True:
+                new_stat = r_os.stat(path)
+                if last_stat != new_stat:
+                    for new_log_line in log_file.read().split('\n'):
+                        self._parse_log_line(new_log_line)
+                    last_stat = new_stat
+                await asyncio.sleep(0) # Pass control to event loop
+        except asyncio.exceptions.CancelledError:
+            pass
+        finally:
+            # Task Destruction
+            log_file.close()
+
+    def _parse_log_line(self, log_line: str):
+        """Parses a new log line.
+
+        Args:
+            log_line: The new log line.
+        """
+
+        assert 'error' in log_line
+
